d82534ad8e2a523616de73fcb22f821d778ff707
Mamady Sillah <supurpisces310@msn.com>
2020-09-09 19:42:29 -0400

    added hero: windstorm to class export on heroes component and and imported onto app.component.html by using <app-heros> open and closing tags

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,13 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.css'],
+  
 })
 export class HeroesComponent implements OnInit {
-
+  hero = 'Windstorm';
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
