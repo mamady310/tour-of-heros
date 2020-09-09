fb5e54e088b54cd8ab73824ff9f48a814227d596
Mamady Sillah <supurpisces310@msn.com>
2020-09-09 19:54:49 -0400

    created hero interface in its own irle in src/app folder

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,15 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
-  
+  styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero = 'Windstorm';
+  hero = "Windstorm"
   constructor() { }
 
   ngOnInit(): void {
-
   }
 
 }
