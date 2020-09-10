d4f85079b3536d5d42fcede41da431fb660bf0e3
Mamady Sillah <supurpisces310@msn.com>
2020-09-10 08:55:58 -0400

    Imported HEROES from mock-heroes into heroes.ts component. Defied a heroes property in the HeroesComponent class

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heros';
 
 @Component({
   selector: 'app-heroes',
@@ -11,7 +12,7 @@ export class HeroesComponent implements OnInit {
     id: 1,
     name: 'Windstorm'
   };
-
+heroes = HEROES;
   constructor() { }
 
   ngOnInit() {
