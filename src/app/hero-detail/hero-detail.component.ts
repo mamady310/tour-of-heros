7e98dbd5f8acb356ce67ea09119c23275494da24
Mamady Sillah <supurpisces310@msn.com>
2020-09-10 10:50:25 -0400

    added Input for hero object under hero-detail class

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input  } from '@angular/core';
 import { Hero } from '../hero';
 
 @Component({
@@ -12,5 +12,7 @@ export class HeroDetailComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  @Input() hero: Hero;
+
 
 }
