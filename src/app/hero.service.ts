7008bd234c0229e548577c2fa08077f956a58195
Mamady Sillah <supurpisces310@msn.com>
2020-09-10 11:37:22 -0400

    imported observable into hero service component

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heros';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
