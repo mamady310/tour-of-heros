6fe094a368957ab1672b38e24d842f1da8fc8c0e
Mamady Sillah <supurpisces310@msn.com>
2020-09-13 17:32:24 -0400

    added function in hero.service.ts to display a message when a hero is fetched

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,18 +2,19 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heros';
 import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
+  // constructor() { }
+  constructor(private messageService: MessageService) { }
 
-  // getHeroes(): Hero[] {
-  //   return HEROES;
-  // }
+  
   getHeroes(): Observable<Hero[]> {
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
